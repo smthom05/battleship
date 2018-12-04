cf809047f558536a9fa0b40afe49a74a46af89b9
Scott Thomas <smthom05@gmail.com>
2018-12-04 11:14:16 -0700

    test_health_goes_down_after_fired_upon completed and passes and corresponding fire_upon method completed

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,20 +1,42 @@
+require 'pry'
+
 class Cell
 
-attr_reader :coordinate, :empty, :ship
+attr_reader :coordinate,
+            :empty,
+            :ship,
+            :fired_upon
 
   def initialize(coordinate)
     @coordinate = coordinate
     @empty = true
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
     @empty
   end
+
   def place_ship(ship)
     @empty = false
     @ship = ship
 
   end
 
-end
+  def fired_upon?
+    @fired_upon
+  end
+
+  def fire_upon
+    @fired_upon = true
+    if @empty == false
+      @ship.hit
+    else
+      @ship
+      # return corresponding symbol
+    end
+  end
+  
+
+  end
