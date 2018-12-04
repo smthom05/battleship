0b90adcc0c68c831258f48a74642eee34ed62d98
Scott Thomas <smthom05@gmail.com>
2018-12-03 18:22:23 -0700

    created and completed the following tests: it_exists, it_has_a_name, it_has_a_length, it_has_health, ship_is_sunk, and ship_is_hit.

---
+++ test/ship_test.rb
@@ -0,0 +1,42 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def setup
+     @ship = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @ship
+  end
+
+  def test_it_has_a_name
+    assert_equal "Cruiser", @ship.name
+  end
+
+  def test_it_has_a_length
+    assert_equal 3, @ship.length
+  end
+
+  def test_it_has_health
+    assert_equal 3, @ship.health
+  end
+
+  def test_ship_is_sunk
+    assert_equal false, @ship.sunk?
+  end
+
+  def test_ship_is_hit
+    assert_equal 3, @ship.health
+    @ship.hit
+    assert_equal 2, @ship.health
+  end
+
+
+
+
+
+end
