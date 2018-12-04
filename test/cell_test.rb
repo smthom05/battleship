2b4048bb3ff486a2b6ec24cc63d3d5ef4d5e5443
Zach Leach <zachleach@techoverco.com>
2018-12-04 08:41:00 -0700

    Completed tests for coordinates and ship instances

---
+++ test/cell_test.rb
@@ -0,0 +1,36 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+
+class CellTest < Minitest::Test
+
+  def setup
+     @cell = Cell.new("B4")
+  end
+
+
+  def test_cell_exists
+    assert_instance_of Cell, @cell
+    end
+
+  def test_cell_has_a_coordinate
+    assert_equal "B4", @cell.coordinate
+  end
+
+  def test_cell_has_a_ship
+    assert_equal nil, @cell.ship
+  end
+
+  def test_cell_can_be_empty
+    assert_equal true, @cell.empty?
+  end
+
+  def test_ship_can_be_placed
+    cruiser = Ship.new("Cruiser", 3)
+    assert_instance_of Ship, @cell.place_ship(cruiser)
+  end
+
+
+end
