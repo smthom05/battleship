88212f7ddc2c21d2290b5088f5f58fb6a2acce93
Scott Thomas <smthom05@gmail.com>
2018-12-04 18:31:23 -0700

    Tested coordinate validation for board class.

--- test/board_test.rb
+++ test/board_test.rb
@@ -14,14 +14,26 @@ class BoardTest < Minitest::Test
   end
 
   def test_it_exists
+    skip
     assert_instance_of Board, @board
   end
 
   def test_it_a_cell
+    skip
     assert_instance_of Cell, @board.cells[:A1]
   end
 
   def test_it_has_16_cells
+    skip
     assert_equal 16, @board.cells.count
+  end
+
+  def test_coordinate_is_on_board
+
+    assert_equal true, @board.valid_coordinate?('A1')
+    assert_equal true, @board.valid_coordinate?('D4')
+    assert_equal false, @board.valid_coordinate?('A5')
+    assert_equal false, @board.valid_coordinate?('E1')
+    assert_equal false, @board.valid_coordinate?('A22')
   end
 end
